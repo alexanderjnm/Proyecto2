9a650fcbfc220e48a79d4f5441456a86517a286e
alexanderjnm <alexanderjnm@hotmail.com>
2023-01-05 21:57:08 -0600

    agregando el boton de modificar

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ document.addEventListener('DOMContentLoaded', getTodos);
 todoButton.addEventListener('click', addTodo);
 todoList.addEventListener('click', deleteCheck);
 filterOption.addEventListener('click', filterTodo);
+todoList.addEventListener('click', editTodo);
 
 //Functions
 
@@ -35,6 +36,11 @@ function addTodo(event){
   trashButton.innerHTML = '<i class="fas fa-trash"></i>';
   trashButton.classList.add("trash-btn");
   todoDiv.appendChild(trashButton);
+  //check edit button
+  const editButton = document.createElement('button');
+  editButton.innerHTML = '<i class="fas fa-edit"></i>';
+  editButton.classList.add("edit-btn");
+  todoDiv.appendChild(editButton);
   //APPEND TO LIST
   todoList.appendChild(todoDiv);
   //clear todo input value
@@ -52,7 +58,7 @@ function deleteCheck(e){
     todo.addEventListener('transitionend', function() {
       todo.remove();
     });
-      
+
   }
   //check mark
   if(item.classList[0] === "complete-btn"){
@@ -122,8 +128,13 @@ function getTodos(){
   //check trash button
   const trashButton = document.createElement('button');
   trashButton.innerHTML = '<i class="fas fa-trash"></i>';
-  trashButton.classList.add("trash-btn");
+  trashButton.classList.add("trash-btn");  
   todoDiv.appendChild(trashButton);
+  //check edit button
+  const editButton = document.createElement('button');
+  editButton.innerHTML = '<i class="fas fa-edit"></i>';
+  editButton.classList.add("edit-btn");
+  todoDiv.appendChild(editButton);
   //APPEND TO LIST
   todoList.appendChild(todoDiv);
   });
@@ -139,4 +150,41 @@ function removeLocalTodos(todo){
   const todoIndex = todo.children[0].innerText;
   todos.splice(todos.indexOf(todoIndex), 1);
   localStorage.setItem("todos",JSON.stringify(todos));
+}
+function editTodo(e) {
+  const item = e.target;
+  if(item.classList[0] === "edit-btn"){
+    const todo = item.parentElement;
+    // Get the to-do text
+    const todoText = todo.firstChild.innerText;
+    // Create an input field for the new to-do text
+    const inputField = document.createElement("input");
+    inputField.value = todoText;
+    inputField.classList.add("todo-input");
+    // Replace the to-do text with the input field
+    todo.firstChild.innerText = "";
+    todo.firstChild.appendChild(inputField);
+    // Focus the input field
+    inputField.focus();
+    // Add a "submit" event listener to the input field
+    inputField.addEventListener("keyup", function(event) {
+      if(event.keyCode === 13) {
+        // Get the new to-do text from the input field
+        const newTodoText = inputField.value;
+        // Update the to-do text
+        todo.firstChild.innerText = newTodoText;
+        // Update the to-do item in local storage
+        updateLocalTodos(todoText, newTodoText);
+      }
+    });
+  }
+}
+
+function updateLocalTodos(oldTodo, newTodo) {
+  let todos = JSON.parse(localStorage.getItem('todos'));
+  // Find the index of the old to-do item
+  const todoIndex = todos.indexOf(oldTodo);
+  // Replace the old to-do item with the new one
+  todos[todoIndex] = newTodo;
+  localStorage.setItem('todos', JSON.stringify(todos));
 }
